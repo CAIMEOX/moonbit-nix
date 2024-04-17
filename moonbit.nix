87750218986837ae870a24639d5ec6ecd0f2537e
CAIMEOX <1853884864@qq.com>
2024-04-17 11:09:51 +0800

    core

--- moonbit.nix
+++ moonbit.nix
@@ -1,26 +1,23 @@
-{ stdenv, pkgs, fetchurl, fetchgit, lib }:
+{ stdenv, pkgs, fetchurl, fetchzip, lib }:
 let cli = "https://cli.moonbitlang.com";
 in stdenv.mkDerivation {
   pname = "moonbit";
-  version = "0.1.0";
-  src = fetchgit {
-    url = "https://github.com/moonbitlang/core";
-    hash = "sha256-KFhRRLHw12UlRemr+zWoHFw/qfusP1OUXnGj8ApKYac=";
+  version = "0.1.0.20240415";
+  src = fetchzip {
+    url = "${cli}/core.zip";
+    hash = "sha256-ky5VD5gAuU9IOTOKRx3pLodg0zRqCmIq90Q5rvYCgIQ=";
   };
   srcs = [
     (fetchurl {
       url = "${cli}/ubuntu_x86/moon";
-      name = "moon";
       hash = "sha256-8k92he/PEWXA3/lMQ84yH4wYCX93uFiYnjfcy7fGhwQ=";
     })
     (fetchurl {
       url = "${cli}/ubuntu_x86/moonc";
-      name = "moonc";
       hash = "sha256-r8S6GVfz4w6mhNbKW2sjcIZ1+Vhsxs/0pNV6QwRHcYk=";
     })
     (fetchurl {
       url = "${cli}/ubuntu_x86/moonfmt";
-      name = "moonfmt";
       hash = "sha256-pPj5cuzRqZVDDRVqeUh6cBZJW1JIGRcwI/+TqxRrUdc=";
     })
     (fetchurl {
@@ -36,26 +33,20 @@ in stdenv.mkDerivation {
       hash = "sha256-BBT3Lx7kIYnn9WjwT5sSt2BrRiLKpimj/wSXt1z5xhE=";
     })
   ];
-  buildInputs = [ pkgs.gcc stdenv.cc pkgs.glibc ];
   preFixup = ''
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/*'';
   doCheck = false;
   sourceRoot = ".";
   dontUnpack = true;
   installPhase = ''
-    cat env-vars
-    runHook preInstall
     mkdir -p $out/lib
+    cp -r $src $out/lib/core
     mkdir -p $out/bin
-    read -ra array <<< "$srcs"
-    cp -r ''${src} $out/lib/core
-    install -m755 -D ''${array[0]} $out/bin/moon
-    install -m755 -D ''${array[1]} $out/bin/moonc
-    install -m755 -D ''${array[2]} $out/bin/moonfmt
-    install -m755 -D ''${array[3]} $out/bin/moonrun
-    install -m755 -D ''${array[4]} $out/bin/moondoc
-    install -m755 -D ''${array[5]} $out/bin/mooninfo
-    runHook postInstall
+    read -ra bins <<< "$srcs"
+    moonbins=(moon moonc moonfmt moonrun moondoc mooninfo)
+    for i in {0..5}; do
+      install -m755 -D ''${bins[$i]} $out/bin/''${moonbins[$i]}
+    done
   '';
   meta = with lib; {
     homepage = "https://www.moonbitlang.com";
